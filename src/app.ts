f06442f66f0f29a09700fb74cdb8d53e7ebec166
Alan <tiltedveggie@outlook.kr>
2023-05-13 11:59:12 -0300

    Install morgan module type

--- src/app.ts
+++ src/app.ts
@@ -5,31 +5,34 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import cors from 'cors';
 import path from 'path';
+
 import indexRoutes from './routes/index.routes';
+import tasksRoutes from './routes/tasks.routes';
+import aboutRoutes from './routes/about.routes';
 
 import db from './database';
 db();
+
+import morgan from 'morgan';
+
+// ---------------------------------------------------------
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 // ---------------------------------------------------------
 
-// Define where is the "views" folder. For multi-plataform purposes, I will use
-// the environment variable "__dirname" that tells us the absolute path of the
-// directory containing the currently executing file. Also I will use the "path"
-// Node module, basically let us concatenate (using join() method) directories and,
-// depending the OS, it will use the "/" or "\" symbol.
 app.set('views', path.join(__dirname, 'views'));
 
-// Configure templates reader
 app.engine(
 	'.hbs',
 	engine({
-		layoutsDir: path.join(app.get('views'), 'layouts'), // In "layouts" directory, inside "views" directory, there is a file with code in common for all pages
-		partialsDir: path.join(app.get('views'), 'partials'), // There is a "partials" directory that contains small pieces of code for import in another .hbs files
+		layoutsDir: path.join(app.get('views'), 'layouts'),
+		partialsDir: path.join(app.get('views'), 'partials'),
 		defaultLayout: 'main',
 		extname: '.hbs'
 	})
@@ -39,6 +42,10 @@ app.set('view engine', '.hbs');
 
 // ---------------------------------------------------------
 
-app.use(indexRoutes);
+app.use('/', indexRoutes);
+app.use('/tasks', tasksRoutes);
+app.use('/about', aboutRoutes);
+
+// ---------------------------------------------------------
 
 export default app;
